c78667f3e98c99c6b2f66802f6ee4a4445cf8bed
nandansoftechindia <self.ram100@gmail.com>
2019-07-20 17:13:00 +0530

    no message

--- Assignment/src/test/java/restassuredfile/GetCall.java
+++ Assignment/src/test/java/restassuredfile/GetCall.java
@@ -1,4 +1,28 @@
 package restassuredfile;
 
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 public class GetCall {
+    public static void main(String[] args) {
+        RestAssured.baseURI = "https://reqres.in/api/users";
+        RequestSpecification httpRequest = RestAssured.given();
+
+        Response response = httpRequest.request(Method.GET, "/2");
+
+        String responseBody = response.getBody().asString();
+
+        System.out.println("Response Body is: " + responseBody);
+
+        int statusCode = response.getStatusCode();
+
+        System.out.println("statusCode is: " + statusCode);
+
+        String statuLine = response.getStatusLine();
+
+        System.out.println("StatusLine is: " + statuLine);
+
+    }
 }
--- Assignment/src/test/java/restassuredfile/PostCall.java
+++ Assignment/src/test/java/restassuredfile/PostCall.java
@@ -1,4 +1,36 @@
 package restassuredfile;
 
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.simple.JSONObject;
+
 public class PostCall {
+
+    public static void main(String[]args)
+    {
+        RestAssured.baseURI="https://reqres.in/api";
+        RequestSpecification httpRequest=RestAssured.given();
+
+        JSONObject requestParams=new JSONObject();
+
+        requestParams.put("name","RamNandan");
+        requestParams.put("job","Engineer");
+
+        httpRequest.header("Content-Type","application/json");
+        httpRequest.body(requestParams.toJSONString()); // Attach json data to the request
+
+        Response response=httpRequest.request(Method.POST,"/users");
+
+        String responseBody=response.getBody().asString();
+
+        System.out.println("Response Body is: "+responseBody);
+
+        int statuscode=response.getStatusCode();
+
+        System.out.println("Status code is: "+statuscode);
+
+
+    }
 }
